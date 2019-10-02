09fc7dfdb35933825ab10d579ed430e789981d1c
Marc Settin Jr <30703608+marcsettin@users.noreply.github.com>
2019-10-02 19:24:56 -0400

    Bridge Control

---
+++ Bridge Control/Bridge Control/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Bridge Control
+//
+//  Created by Marc Settin on 10/24/18.
+//  Copyright © 2018 Marc Settin. All rights reserved.
+//
+
+import Foundation
+
+let officerKey = "officer"
+let authorizationCodeKey = "authorizationCode"
+let rankKey = "rank"
+let warpDriveKey = "warp"
+let warpFactorKey = "warpFactor"
+let favoriteTeaKey = "favoriteTea"
+let favoriteCaptainKey = "favoriteCaptain"
+let favoriteGadgetKey = "favoriteGadget"
+let favoriteAlienKey = "favoriteAlien"
