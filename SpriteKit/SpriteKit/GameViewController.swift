d2e74c64979fa37f0de69bd85534d980f47d4a5b
Marc Settin Jr <30703608+marcsettin@users.noreply.github.com>
2019-10-02 19:30:11 -0400

    My first iPhone game!

---
+++ SpriteKit/SpriteKit/GameViewController.swift
@@ -0,0 +1,52 @@
+//
+//  GameViewController.swift
+//  SpriteKitGame
+//  Created by Marc Settin on 11/20/18.
+//  Copyright © 2018 Marc Settin. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let scene = GameScene(fileNamed:"GameScene") {
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .aspectFit
+            
+            skView.presentScene(scene)
+        }
+    }
+
+    override var shouldAutorotate : Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
+}
---
+++ SpriteKit/SpriteKit/WaitingForTap.swift
@@ -0,0 +1,35 @@
+//
+//  WaitingForTap.swift
+//  SpriteKitGame
+//  Created by Marc Settin on 11/20/18.
+//  Copyright © 2018 Marc Settin. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class WaitingForTap: GKState {
+  unowned let scene: GameScene
+  
+  init(scene: SKScene) {
+    self.scene = scene as! GameScene
+    super.init()
+  }
+  
+  override func didEnter(from previousState: GKState?) {
+    let scale = SKAction.scale(to: 1.0, duration: 0.25)
+    scene.childNode(withName: GameMessageName)!.run(scale)
+  }
+  
+  override func willExit(to nextState: GKState) {
+    if nextState is Playing {
+      let scale = SKAction.scale(to: 0, duration: 0.4)
+      scene.childNode(withName: GameMessageName)!.run(scale)
+    }
+  }
+  
+  override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+    return stateClass is Playing.Type
+  }
+
+}
